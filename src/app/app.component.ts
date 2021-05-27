4604ab67249fdde8a10d8b496b2ac9647f7ff805
Rafael Freitas Costa <rafaelevoinfodeveloper@gmail.com>
2021-05-26 23:04:43 -0300

    Home finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 import firebase from 'firebase/app';
+// import {Router} from '@angular/core/r';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,21 @@ import firebase from 'firebase/app';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private auth: AngularFireAuth) {
+  constructor(private auth: AngularFireAuth, private router:Router) {
     this.auth.signInWithEmailAndPassword("anonimo@gmail.com", '123456');
   }
 
   login() {
 
+  }
+
+  radioChanged(event){
+    if ((event) && (event.target)){
+      let vaLink = event.target.getAttribute('data-link');
+      if (vaLink){
+        this.router.navigateByUrl(vaLink);
+      }
+    }
+    
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,17 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
-  abrirFace() {
-    window.location.href = "https://www.facebook.com/XadrezMineiros-101407585326095";
-  }
-
-  abrirInstagram() {
-    window.location.href = "https://www.instagram.com/xadrezmineiros/";
+  abrirTorneios(){  
+    //this.router.navigateByUrl('torneios');
   }
 
 }
